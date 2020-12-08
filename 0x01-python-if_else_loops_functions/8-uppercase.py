620dca3bed55ac07a23eb1f19d460355c6abbe17
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2020-12-08 17:06:05 -0500

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 # Python function that prints a string
 # in uppercase followed by a new line.
-
-
 def uppercase(str):
-    for u in str:
-        print("{}".format(chr(ord(u) - 32)
-                          if (ord(u) >= ord("a") and
-                              ord(u) <= ord("z")) else u), end="")
+    for c in str:
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
+            c = chr(ord(c) - ord(' '))
+        print('{:s}'.format(c), end='')
     print('\n', end='')
