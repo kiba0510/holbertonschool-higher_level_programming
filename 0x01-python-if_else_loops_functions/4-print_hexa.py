dc5fc86262fd019cdbf07b41752d3aea0adb8c43
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2020-12-08 14:17:28 -0500

    Update 4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Prints '0' to '98' in decimal and 
+# Prints '0' to '98' in decimal and
 # it's equivalent in hexadecimal
 for i in range(0, 99):
     print("{:d} = 0x{:x}".format(i, i))
