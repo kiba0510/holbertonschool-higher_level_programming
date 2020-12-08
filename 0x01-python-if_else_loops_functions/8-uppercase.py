a9aaeed7d9e3e81425a2298ce70ad0e458aa6f13
kiba0510 <kiba0510@gmail.com>
2020-12-08 22:09:12 +0000

    hotfix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# Python function that prints a string
-# in uppercase followed by a new line.
 def uppercase(str):
     for c in str:
         if ord(c) >= ord('a') and ord(c) <= ord('z'):
