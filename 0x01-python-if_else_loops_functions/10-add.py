e2306367023a703a37d2a5c3f640d82a3334ab36
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2020-12-08 19:31:02 -0500

    Update 10-add.py
    pep8 hotfix

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    if (number < 0):
-        number = number * -1
-    print(number % 10, end="")
-    return (number % 10)
+def add(a, b):
+    return (a + b)
