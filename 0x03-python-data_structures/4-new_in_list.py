a17fa3899c4e351598431ff41ae3547721fe5fd3
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2020-12-15 15:20:22 -0500

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,5 +2,5 @@
 def new_in_list(my_list, idx, element):
     if (idx >= 0 and idx <= len(my_list) - 1):
         my_list = my_list.copy()
-        my_list [idx] = element
+        my_list[idx] = element
     return (my_list)
