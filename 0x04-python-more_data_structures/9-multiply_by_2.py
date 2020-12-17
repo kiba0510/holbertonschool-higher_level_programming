60a1691e3741ab580bf830991e4ac20f9292995a
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2020-12-17 18:06:52 -0500

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,5 +3,5 @@ def multiply_by_2(a_dictionary):
     new_dict = {}
     if a_dictionary:
         for k, v in a_dictionary.items():
-            new_dict.update({k: v * 2})
+            new_dict[k] = v * 2
         return (new_dict)
