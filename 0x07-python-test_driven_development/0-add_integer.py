ca2dad35316b1dd611c9f755d875814ccec24f09
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2021-01-10 11:57:30 -0500

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,8 +7,8 @@ def add_integer(a, b):
     """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a most be an integer")
-    
+
     if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    
+
     return int(a + b)
