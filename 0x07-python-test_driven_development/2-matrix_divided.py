82438be8755a3aaca5affbd24dcc400adc0023a1
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2021-01-10 12:54:14 -0500

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,8 +8,6 @@ def matrix_divided(matrix, div):
     """
     a function to divide a matrix by a constant
     """
-    
-    
     if type(matrix) is not list or len(matrix) is 0:
         raise TypeError("matrix must be a matrix\
         (list of lists) of integers/floats")
