11fafc4105f4ebf22ef8b79e1d81d24fa9caae06
kiba0510 <kiba0510@gmail.com>
2021-01-20 03:36:51 +0000

    Python functio added

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defining a geometry class
+"""
+
+class BaseGeometry:
+    def area(self):
+        """
+        Function that calculates the area
+        """
+        raise Exception("area() is not implemented")
