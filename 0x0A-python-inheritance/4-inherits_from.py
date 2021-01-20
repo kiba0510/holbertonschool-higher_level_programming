53d35e8ca80920f8e19d79bfae7b4d5048a16047
kiba0510 <kiba0510@gmail.com>
2021-01-20 03:27:56 +0000

    Python functio added

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Defining inherits_from
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    returns True if the object is an instance of a class that inherited
+    from the specified class, otherwise False
+    """
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return True
+    return False
