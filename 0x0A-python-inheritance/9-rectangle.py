9a7e929b2e7b35d1b6f630d2345f7f871c7caf2e
kiba0510 <kiba0510@gmail.com>
2021-01-20 19:48:08 +0000

    Python function added

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Implementing a new class called Rectangle
+"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''
+    Class Rectangle inheriting BaseGeometry class attributes
+    '''
+    def __init__(self, width, height):
+        '''
+        Initializing Rectangle class
+        '''
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        '''
+        Defining area methd
+        '''
+        return self.__width * self.__height
+
+    def __str__(self):
+        '''
+        Defining str method
+        '''
+        return ("[{}] {}/{}".format(type(self).__name__,
+                                   self.__width, self.__height))
