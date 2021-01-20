1a0097a05430deba70d292e32c24520621fa38f5
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2021-01-20 14:54:25 -0500

    Update 9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -29,4 +29,5 @@ class Rectangle(BaseGeometry):
         '''
         Defining str method
         '''
-        return ("[{}] {}/{}".format(type(self).__name__, self.__width, self.__height))
+        return ("[{}] {}/{}".format(type(self).__name__,\
+                                    self.__width, self.__height))
