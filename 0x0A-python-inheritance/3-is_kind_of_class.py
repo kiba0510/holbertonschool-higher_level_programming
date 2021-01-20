b039d5529290c4f4d5c44bce84d903b32c7775f0
kiba0510 <kiba0510@gmail.com>
2021-01-20 03:20:54 +0000

    Python function added

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Defining is_kind_of_class function
+"""
+
+def is_kind_of_class(obj, a_class):
+    """
+    Checks the type of class and inherited class
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
