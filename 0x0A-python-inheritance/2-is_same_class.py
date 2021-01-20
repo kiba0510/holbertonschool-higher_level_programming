b8b6a28f4abbe3d0a99b36e0a6135df48757933f
kiba0510 <kiba0510@gmail.com>
2021-01-20 03:08:42 +0000

    Python function added

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Defining is_same_class function
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Function is_same_class
+    """
+    if type(obj) is a_class:
+        return True
+    return False
