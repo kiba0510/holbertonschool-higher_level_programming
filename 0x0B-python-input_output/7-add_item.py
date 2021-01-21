697a1a3f2a248fd5ce6488e2b31bd29861a704da
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2021-01-20 23:14:29 -0500

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,31 +1,12 @@
 #!/usr/bin/python3
-"""
-Defining add_item
-"""
-
-
+'''module add_item'''
 import sys
-save_to_json = __import__("7-save_to_json_file").save_to_json_file
-load_from_json = __import__("8-load_from_json_file").load_from_json_file
-
-
-def add_item(args, filename):
-    '''
-    adds items to a json file
-    @args: The arguments that need to be added.
-    @filename: The file that needs to be updated
-    '''
-    try:
-        content = load_from_json(filename)
-    except:
-        content = []
-
-    for item in args:
-        content.append(item)
-    save_to_json(content, filename)
-
-
-if __name__ == "__main__":
-    args = sys.argv[1:]
-    filename = "add_item.json"
-    add_item(args, filename)
+save = __import__('7-save_to_json_file').save_to_json_file
+load = __import__('8-load_from_json_file').load_from_json_file
+try:
+    file = load("add_item.json")
+except FileNotFoundError:
+    file = []
+for i in sys.argv[1:]:
+    file.append(i)
+save(file, "add_item.json")
