5686a4ea2ffe09245ff8a111a840f53495579b6d
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2021-01-20 23:19:29 -0500

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-'''module add_item'''
-import sys
-save = __import__('7-save_to_json_file').save_to_json_file
-load = __import__('8-load_from_json_file').load_from_json_file
+'''Script hat adds all arguments
+to a Python list, and then save them to a file'''
+from sys import argv
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+i = len(argv)
+
 try:
-    file = load("add_item.json")
-except FileNotFoundError:
-    file = []
-for item in sys.argv[1:]:
-    file.append(item)
-save(file, "add_item.json")
+    list_arguments = load_from_json_file("add_item.json")
+except:
+    list_arguments = []
+
+for counter in range(1, i):
+    list_arguments.append(argv[counter])
+
+save_to_json_file(list_arguments, "add_item.json")
