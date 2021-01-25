e6f028dc7a84a55198d4ad0106e0fafb5aa5d408
kiba0510 <kiba0510@gmail.com>
2021-01-25 23:32:59 +0000

    added area and display

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -27,12 +27,86 @@ class Rectangle(Base):
             '''
             return self.__width
 
-            @width.setter
-            def width(self, value):
-                '''
-                Defining setter of width
-                '''
-                if not isinstance(width, int):
-                    raise TypeError("width must be an integer")
-                if width <= 0:
-                    raise ValueError("width must be > 0")
+        @width.setter
+        def width(self, value):
+            '''
+            Defining setter of width
+            '''
+            if not isinstance(width, int):
+                raise TypeError("width must be an integer")
+            if width <= 0:
+                raise ValueError("width must be > 0")
+            self.__width = width
+
+        @property
+        def height(self):
+            '''
+            Defining Getter of height
+            '''
+            return self.__height
+
+        @height.setter
+        def height(self, height):
+            '''
+            Defining getter of height
+            '''
+            if not isinstance(height, int):
+                raise TypeError("height must be an integer")
+            if height <= 0:
+                raise ValueError("height must be > 0")
+            self.__height = height
+
+        @property
+        def x(self):
+            '''
+            Defining getter of x
+            '''
+            return self.__x
+
+        @x.setter
+        def x(self, x):
+            '''
+            defining setter of x
+            '''
+            if not isinstance(x, int):
+                raise TypeError("x must be an integer")
+            if x < 0:
+                raise ValueError("x must be >= 0")
+            self.__x = x
+
+        @property
+        def y(self):
+            '''
+            Defining getter of y
+            '''
+            return self.__y
+
+        @y.setter
+        def y(self, y):
+            '''
+            Defining setter of y
+            '''
+            if not isinstance(y, int):
+                raise TypeError("y must be an integer")
+            if y < 0:
+                raise ValueError("y must be >= 0")
+            self.__y = y
+
+        def area(self):
+            '''
+            Return the area value of the Rectangle
+            '''
+            return self.__width * self.__height
+
+        def display(self):
+            '''
+            Prints the STDOUT of the Rectangle with #
+            '''
+            for y in range(self.y):
+                print()
+            for height in range(self.height):
+                for x in range(self.x):
+                    print(" ", end="")
+                for width in range(self.width):
+                    print('#', end="")
+                print()
