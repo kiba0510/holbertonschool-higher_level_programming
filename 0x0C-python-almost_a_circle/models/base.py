562f916f93e03ffed6ae4822b80b9757bcfbd321
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2021-01-27 10:57:10 -0500

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 '''This is the BASE class of all other classes'''
 
+
 class Base:
     '''
-    Manage id attribute in all future classes to avoid duplicating the same code
+    Manage id attribute in all future classes 
+    to avoid duplicating the same code
     '''
     __nb_objects = 0
 
     def __init__(self, id=None):
-
 
         if id is not None:
             self.id = id
