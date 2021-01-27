8dd6d416178eff4a301963ca7517d8cc94c96f83
kiba0510 <kiba0510@gmail.com>
2021-01-27 19:39:19 +0000

    hotfix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,21 +9,36 @@ class Rectangle(Base):
     '''
     Defining class Rectangle
     '''
-
     def __init__(self, width, height, x=0, y=0, id=None):
         '''
-        Initiating class Rectangle
+        Initiating Rectangle class
         '''
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
         super().__init__(id)
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        if type(height) is not int:
+            raise TypeError('height must be an integer')
+        if type(x) is not int:
+            raise TypeError('x must be an integer')
+        if type(y) is not int:
+            raise TypeError('y must be an integer')
+        if width <= 0:
+            raise ValueError('width must be > 0')
+        if height <= 0:
+            raise ValueError('height must be > 0')
+        if x < 0:
+            raise ValueError('x must be >= 0')
+        if y < 0:
+            raise ValueError('y must be >= 0')
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
     @property
     def width(self):
         '''
-        Definig getter of width
+        Defining getter of width
         '''
         return self.__width
 
@@ -33,28 +48,28 @@ class Rectangle(Base):
         Defining setter of width
         '''
         if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if width <= 0:
-            raise ValueError("width must be > 0")
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
         self.__width = value
 
     @property
     def height(self):
         '''
-        Defining Getter of height
+        Defining getter of height
         '''
         return self.__height
 
     @height.setter
-    def height(self, height):
+    def height(self, value):
         '''
-        Defining getter of height
+        Defining setter of height
         '''
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        if height <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = height
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height must be > 0')
+        self.__height = value
 
     @property
     def x(self):
@@ -64,15 +79,15 @@ class Rectangle(Base):
         return self.__x
 
     @x.setter
-    def x(self, x):
+    def x(self, value):
         '''
-        defining setter of x
+        Defining stter of x
         '''
-        if not isinstance(x, int):
-            raise TypeError("x must be an integer")
-        if x < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = x
+        if type(value) is not int:
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
+        self.__x = value
 
     @property
     def y(self):
@@ -82,15 +97,15 @@ class Rectangle(Base):
         return self.__y
 
     @y.setter
-    def y(self, y):
+    def y(self, value):
         '''
         Defining setter of y
         '''
-        if not isinstance(y, int):
-            raise TypeError("y must be an integer")
-        if y < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = y
+        if type(value) is not int:
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be >= 0')
+        self.__y = value
 
     def area(self):
         '''
@@ -102,19 +117,61 @@ class Rectangle(Base):
         '''
         Prints the STDOUT of the Rectangle with #
         '''
-        for y in range(self.y):
+        for i in range(self.__y):
             print()
-        for height in range(self.height):
-            for x in range(self.x):
-                print(" ", end="")
-            for width in range(self.width):
-                print('#', end="")
-            print()
+        for i in range(self.__height):
+            print(' ' * self.__x + '#' * self.__width)
 
     def __str__(self):
         '''
-        Override the __str__ method
+        Override the __str__method
         '''
-        str1 = '(' + str(self.id) + ') ' + str(self.__x) + '/'
-        str2 = str(self.__y) + ' - ' + str(self.__width)
-        return "[Rectangle] " + str1 + str2 + "/" + str(self.__height)
+        str0 = "[" + self.__class__.__name__ + "] (" + str(self.id) + ") "
+        str1 = str(self.__x) + "/" + str(self.__y) + " - " + str(self.__width)
+        if self.__class__.__name__ == 'Square':
+            return str0 + str1
+        return str0 + str1 + "/" + str(self.__height)
+
+    def update(self, *args, **kwargs):
+        '''
+        Object to Dictionary
+        '''
+        if args is None or len(args) == 0:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'width' in kwargs:
+                self.__width = kwargs['width']
+            if 'height' in kwargs:
+                self.__height = kwargs['height']
+            if 'x' in kwargs:
+                self.__x = kwargs['x']
+            if 'y' in kwargs:
+                self.__y = kwargs['y']
+        else:
+            if len(args) < 1:
+                return
+            self.id = args[0]
+            if len(args) < 2:
+                return
+            self.__width = args[1]
+            if len(args) < 3:
+                return
+            self.__height = args[2]
+            if len(args) < 4:
+                return
+            self.__x = args[3]
+            if len(args) < 5:
+                return
+            self.__y = args[4]
+
+    def to_dictionary(self):
+        '''
+        Rectangle to Dictionary
+        '''
+        d = {}
+        d.setdefault('id', self.id)
+        d.setdefault('width', self.__width)
+        d.setdefault('height', self.__height)
+        d.setdefault('x', self.__x)
+        d.setdefault('y', self.__y)
+        return d
