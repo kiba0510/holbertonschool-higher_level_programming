a08e95313adccae6e72f6facc4bdb02b67529c04
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2021-01-27 11:01:30 -0500

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,10 +14,10 @@ class Rectangle(Base):
         '''
         Initiating class Rectangle
         '''
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
         super().__init__(id)
 
     @property
