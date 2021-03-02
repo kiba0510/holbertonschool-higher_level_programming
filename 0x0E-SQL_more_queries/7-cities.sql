18c671cea9a08b99bb50c21b5bfebe5e090ef41f
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2021-03-02 15:06:52 -0500

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,8 @@
 -- Creates the database hbtn_0d_usa and the table cities on your MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
-(id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-state_id INT NOT NULL, FOREIGN KEY(state_id) 
-REFERENCES state(id), name VARCHAR(256) NOT NULL;
+CREATE TABLE IF NOT EXISTS cities(`id` INT UNIQUE AUTO_INCREMENT NOT NULL,
+                                  `state_id` INT NOT NULL,
+                                  `name` VARCHAR(256),
+                                  PRIMARY KEY(`id`),
+                                  FOREIGN KEY(`state_id`) REFERENCES states(`id`));
