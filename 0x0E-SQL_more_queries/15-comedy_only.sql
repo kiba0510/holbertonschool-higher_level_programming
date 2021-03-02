332b446b11b0a160dffe7259f9f97751ada478a0
kiba0510 <kiba0510@gmail.com>
2021-03-02 14:48:37 -0500

    task 0 to 16 added

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- Lists all Comedy shows in the database hbtn_0d_tvshows
+SELECT tv_shows.title
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;
