555bc6fea78f53109c85fdb4dd3fa4e1aae9bdd3
kiba0510 <kiba0510@gmail.com>
2021-03-22 13:01:24 -0500

    task 6 hotfix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -4,4 +4,4 @@
 (like 1-multi_languages.js) but by using an array of string and a loop */
 let myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
-for (let i = 0, len = myArray.length; i < len; i++) console.log(myArray[i]);
+for (let i = 0, myArray[i] !== undefined; i++) console.log(myArray[i]);
