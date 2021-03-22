6ad81f72e2d31dac4f68045d42bb5957062b8332
kiba0510 <kiba0510@gmail.com>
2021-03-22 13:55:15 -0500

    task 7 added

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// script that prints x times “C is fun”
+let num = process.argv[2];
+
+if (!isNaN(parseInt(process.argv[2]))) {
+    for (let i = 0; i < num; i++) {
+        console.log('C is fun');
+    }
+}
+else {
+    console.log(('Missing number of occurrences'));
+}
