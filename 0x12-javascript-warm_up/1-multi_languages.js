28b11c1e8111cce7ee94a84f3815e7bf20f11c0c
kiba0510 <kiba0510@gmail.com>
2021-03-22 11:03:48 -0500

    task 1 added

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+let myVar_C = 'C is fun';
+let myVar_P = 'Python is cool';
+let myVar_J = 'Javascript is amazing';
+
+console.log(myVar_C);
+console.log(myVar_P);
+console.log(myVar_J);
