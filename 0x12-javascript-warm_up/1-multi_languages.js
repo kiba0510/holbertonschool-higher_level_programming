f190fc58d5cc0dccc8fd7b92932d04a2a08d25e2
kiba0510 <kiba0510@gmail.com>
2021-03-22 12:39:09 -0500

    task 1 hotfix

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-let myVar_C = 'C is fun';
-let myVar_P = 'Python is cool';
-let myVar_J = 'Javascript is amazing';
-
-console.log(myVar_C);
-console.log(myVar_P);
-console.log(myVar_J);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
