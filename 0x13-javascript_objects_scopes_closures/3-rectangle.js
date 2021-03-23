efd0899a81c5a72280520e99a8acfb398558b82a
kiba0510 <kiba0510@gmail.com>
2021-03-23 12:02:18 -0500

    added task 0 to 5

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+// Empty class Rectangle that defines a rectangle
+class Rectangle {
+    constructor(w, h) {
+        if (w > 0 && h > 0) { 
+        this.width = w;
+        this.height = h;
+        }
+    }
+
+    print() {
+        for (let i = 0; i < this.height; i++) {
+            let myRow = '';
+            for (let j = 0; j < this.width; j++) {
+                myRow += 'X';
+            }
+            console.log(myRow);
+        }
+    }
+}
+    
+module.exports = Rectangle;
