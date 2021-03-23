b1b207ef02f4f5465c598a08716cab89a609d373
kiba0510 <kiba0510@gmail.com>
2021-03-23 02:18:39 +0000

    style fixed

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,12 +2,12 @@
 
 // script that computes and prints a factorial
 function factorial (number) {
-    if (isNaN(parseInt(number))) {
-        return (1);
-    }
-    if (number === 1) {
-        return (1);
-    }
-    return (number * factorial(number - 1));
+  if (isNaN(parseInt(number))) {
+    return (1);
+  }
+  if (number === 1) {
+    return (1);
+  }
+  return (number * factorial(number - 1));
 }
 console.log(factorial(parseInt(process.argv[2])));
--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -2,5 +2,5 @@
 
 // function that returns the addition of 2 integers
 exports.add = function (num1, num2) {
-    return (num1 + num2);
+  return (num1 + num2);
 };
