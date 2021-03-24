4015c0e9ba1a14ce9812d1de91da1a48e848a163
kiba0510 <kiba0510@gmail.com>
2021-03-24 03:18:59 +0000

    style hotfix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,9 +2,9 @@
 
 // Function that returns the reversed version of a list
 exports.esrever = function (list) {
-    let myList = [];
-    for (let i = list.length - 1; i >= 0; i--) {
-        myList.push(list[i]);
-    }
-    return (myList);
-}
+  const myList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    myList.push(list[i]);
+  }
+  return (myList);
+};
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-/* Function that prints the number of arguments 
+/* Function that prints the number of arguments
 already printed and the new argument value */
 let counter = 0;
 exports.logMe = function (item) {
-    console.log(counter + ': ' + item);
-    counter += 1;
+  console.log(counter + ': ' + item);
+  counter += 1;
 };
