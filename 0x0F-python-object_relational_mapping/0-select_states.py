a2f2bac6c012f41f45e137c67306f9f6243ed1c3
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2021-03-27 17:12:36 -0500

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ import sys
 
 
 if __name__ == "__main__":
-    '''Connect to MySQL server'''
+    """Connect to MySQL server"""
     
     db = MySQLdb.connect(host="localhost",
                          user=sys.argv[1],
@@ -13,17 +13,14 @@ if __name__ == "__main__":
                          db=sys.argv[3],
                          port=3306)
     
-    '''Instatiate a MySQL Curso Object'''
+    """Instatiate a MySQL Curso Object"""
     cursor = db.cursor()
     
-    '''Execute the DataBase operation'''
+    """Execute the DataBase operation"""
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     
-    '''Fetch the row of a query and return a list of tuple'''
     for row in cursor.fetchall():
         print(row)
 
-    '''Close cursor, resets results and ensures that the cursor object
-    has no reference to its original connection object'''
     cursor.close()
     db.close()
