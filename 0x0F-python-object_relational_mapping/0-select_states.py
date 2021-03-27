c2cda740c213ddd69396a18a6caecd043c910d4e
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2021-03-27 16:27:09 -0500

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
     '''
     Script that lists all states from the database hbtn_0e_0_usa
     '''
     db = MySQLdb.connect(host="localhost",
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3],
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
                          port=3306)
 
     cursor = db.cursor()
