592be3d9115eaa2224d1dc4841c5bffd4a4419dd
kiba0510 <kiba0510@gmail.com>
2021-03-28 12:01:09 -0500

    task 14

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+City class - MySQLAlchemy module
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+
+
+class City(Base):
+    """
+    City class
+    """
+
+    __tablename__ = 'cities'
+    id = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
