64f5f308056ef9a7cf09f0bb4f8fbe600d591616
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2021-03-28 10:40:13 -0500

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -4,25 +4,35 @@ Script that prints the first
 State object from the database hbtn_0e_6_usa
 """
 
-import sys
+from sys import argv
 from model_state import Base, State
+
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
-
 if __name__ == "__main__":
-    user_name = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    connection = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    """Function that lists all State objects from the db"""
+    credentials = {
+        "host": "localhost",
+        "port": "3306",
+        "user": argv[1],
+        "pass": argv[2],
+        "db": argv[3]}
 
-    engine = create_engine(connection.format(user_name, password,
-                                             db_name, pool_pre_pint=True))
+    str_format = 'mysql+mysqldb://{user}:{pass}@{host}:{port}/{db}'
+    engine = create_engine(str_format.format(
+        **credentials), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    query = (session.query(State).
-             filter_by(name=search).order_by(State.id).all())    
-    if len(query) == 0:
+
+    states = session.query(State).filter(State.name == state_search).first()
+
+    if not states:
         print("Not found")
-    for item in query:
-        print("{}".format(item.id))
+    else:
+        print("{}".format(states.id))
+
+    session.close()
