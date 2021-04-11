b19a8fb580d56c4b12f625b8f1a9ebd898d9c4a4
kiba0510 <kiba0510@gmail.com>
2021-04-11 10:43:04 -0500

    hotfix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
-"""
-Script that fetches https://intranet.hbtn.io/status
-"""
+""" Python script that fetches https://intranet.hbtn.io/status """
 import urllib.request
 
+
 if __name__ == "__main__":
-    """
-    Body response:
-    - type: <class 'bytes'>
-    - content: b'OK'
-    - utf8 content: OK
-    """
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
+    print('Body response:')
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: {}'.format(html.decode('utf-8')))
