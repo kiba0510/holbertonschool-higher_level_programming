6427ca22fbe0ac7a1b6817db0b6c05b4b6396a1e
kiba0510 <kiba0510@gmail.com>
2021-04-11 10:40:42 -0500

    task 0 added

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-from urllib.request import Request, urlopen
+"""
+Script that fetches https://intranet.hbtn.io/status
+"""
+import urllib.request
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    req = Request(url)
-    with urlopen(req) as response:
-        page = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(page)))
-    print("\t- content: {}".format(page))
-    print("\t- utf8 content: {}".format(page.decode("utf-8")))
+    """
+    Body response:
+    - type: <class 'bytes'>
+    - content: b'OK'
+    - utf8 content: OK
+    """
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
