c931df453e2f3386f3b11a3f32ad8bdd59f1de4a
kiba0510 <kiba0510@gmail.com>
2021-05-04 12:30:31 -0500

    semistandard hotfix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,11 @@
 #!/usr/bin/nodejs
 /*
-- script that prints the title of a Star Wars movie 
+- script that prints the title of a Star Wars movie
 - where the episode number matches a given integer
 */
-const request 
+const request = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+request(url, (error, response, body) => {
+  if (error) throw error;
+  console.log(JSON.parse(body).title);
+});
