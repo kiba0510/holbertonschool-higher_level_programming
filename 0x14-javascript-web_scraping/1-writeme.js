e4c5ed30328e181c534a2f9b3d6f41ddfa1bb7d6
kiba0510 <kiba0510@gmail.com>
2021-05-04 11:27:19 -0500

    task 1 added

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/nodejs
+/*
+- Script that writes a string to a file.
+- fs module: File System.
+*/
+const fs = require('fs');
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
+    if (err) throw err;
+});
