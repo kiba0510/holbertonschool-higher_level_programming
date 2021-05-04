7789d11116033e28a3e69e2317c0c4b657f4a0f4
kiba0510 <kiba0510@gmail.com>
2021-05-04 12:15:03 -0500

    task 2 added

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/nodejs
+/*
+- Script that display the status code of a GET request
+*/
+const request = require('request');
+const info = {
+    url: process.argv[2],
+    method: 'GET'
+};
+request(info, (err, response) => {
+    if (err) throw err;
+    console.log('code:', response.statusCode);
+});
